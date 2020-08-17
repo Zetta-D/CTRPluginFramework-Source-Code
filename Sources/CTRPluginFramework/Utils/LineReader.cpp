b6cd5f0b89af621bfef07abb15fda1de3660bc11
ZettaD <42093520+Zetta-D@users.noreply.github.com>
2020-08-17 11:31:37 +0200

    Update LineReader.cpp

--- Sources/CTRPluginFramework/Utils/LineReader.cpp
+++ Sources/CTRPluginFramework/Utils/LineReader.cpp
@@ -1,76 +1,60 @@
-namespace CTRPluginFramework {
+#include "CTRPluginFramework/Utils/LineReader.hpp"
 
-  LineReader::LineReader(File & file):
-    _file(file), _offsetInBuffer(0), _dataInBuffer(0), _buffer(Heap::Alloc((Heap * ) 0x1000, file)) {}
-
-  LineReader::~LineReader() {
-    Heap::Free(_buffer, _file);
-  }
-
-  bool operator()(std::string & line) {
-    File ** v3; // r4
-    int v4; // r5
-    int v5; // r1
-    __int64 v6; // r10
-    __int64 v7; // r0
-    signed __int64 v8; // r6
-    __int64 v9; // r2
-    _BOOL4 result; // r0
-    File * v11; // r2
-    File * v12; // r3
-    File * v13; // r1
-    int v14; // r1
-    bool v15; // zf
-
-    *(_DWORD * )(line + 4) = 0;
-    v3 = _file;
-    v4 = line;
-    LODWORD(v6) = _file.GetSize();
-    HIDWORD(v6) = v5;
-    do {
-      while (1) {
-        ( * ((_DWORD * ) & (v7))) = File::Tell( * v3);
-        v8 = v6 - v7;
-        v9 = * (_QWORD * )(v3 + 1);
-        if (v6 - v7 < 1)
-          break;
-        if ((unsigned int) v9 < HIDWORD(v9))
-          goto LABEL_6;
-        if (v8 > 4095)
-          LODWORD(v8) = 4096;
-        if (File::Read( * v3, (void * ) v3[3], v8))
-          goto LABEL_19;
-        v3[1] = 0;
-        v3[2] = (File * ) v8;
-      }
-      if ((u32) v9 >= HIDWORD(v9))
-        return *(_DWORD * )(v4 + 4) != 0;
-      LABEL_6:
-        v11 = v3[2];
-      while (True) {
-        v12 = v3[1];
-        if (v12 >= v11)
-          break;
-        v13 = v3[3];
-        v3[1] = (File * )((char * ) v12 + 1);
-        v14 = * ((unsigned __int8 * ) v12 + (_DWORD) v13);
-        if (v14 == 10)
-          return 1;
-        v15 = v14 == 0;
-        if (v14)
-          v15 = v14 == 13;
-        if (!v15) {
-          v4.push_back();
-          goto LABEL_6;
-        }
-      }
+namespace CTRPluginFramework
+{
+    LineReader::LineReader(File & file):    _file(file), _offsetInBuffer(0), _dataInBuffer(0))
+    {
+        Heap::Alloc(0x1000, _buffer);
     }
-    while (v11);
-    LABEL_19:
-      result = 0;
-    v3[2] = 0;
-    v3[1] = 0;
-    return result;
-  }
 
+    LineReader::~LineReader()
+    {
+        Heap::Free(_buffer);
+    }
+
+    bool LineReader::operator()(std::string &line)
+    {
+        u64 size;
+        u32 data, currentPos;
+        do
+        {
+            while (true)
+            {
+                size = _file.GetSize() - _file.Tell();
+                if (_file.GetSize() - _file.Tell() < 1)
+                    break;
+                if (_offsetInBuffer < _dataInBuffer)
+                    goto LABEL_6;
+                if (size > 0xFFF)
+                    size = 0x1000;
+                if (_file.Read((void*)_buffer, size))
+                    goto LABEL_19;
+                _offsetInBuffer = 0;
+                _dataInBuffer = size;
+            }
+            if (_offsetInBuffer >= _dataInBuffer)
+                return line[4] != 0;
+            LABEL_6:
+                data = _dataInBuffer;
+            while (true)
+            {
+                if (_offsetInBuffer >= data)
+                    break;
+                _offsetInBuffer = &_buffer;
+                currentPos = _offsetInBuffer + &_buffer;
+                if (currentPos == 10)
+                    return true;
+                if (!(currentPos ? currentPos == 13 : currentPos == 0))
+                {
+                    line.push_back();
+                    goto LABEL_6;
+                }
+            }
+        }
+        while (data);
+        LABEL_19:
+        _dataInBuffer = 0;
+        _offsetInBuffer = 0;
+        return false;
+    }
 }
