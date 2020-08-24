29811301c1e15b3f3485a18dbffcb7b6f4dd7f63
Yoshishee <dub.raph@outlook.fr>
2020-08-24 22:08:04 +0200

    Fixed Some Issues

--- Sources/ctrulib/util/rbtree/rbtree_empty.c
+++
@@ -1,7 +0,0 @@
-#include "ctrulib/util/rbtree.h"
-
-int
-rbtree_empty(const rbtree_t *tree)
-{
-  return tree->root == NULL;
-}
--- Sources/ctrulib/util/rbtree/rbtree_size.c
+++
@@ -1,7 +0,0 @@
-#include "ctrulib/util/rbtree.h"
-
-size_t
-rbtree_size(const rbtree_t *tree)
-{
-  return tree->size;
-}
